59e8e6d5e5ad361c86aabdb4b318edd501f9fe7f
Julien Castets <castets.j@gmail.com>
2014-08-13 12:27:39 +0200

    Add cli.arguments_parser

--- dyntftpd/cli.py
+++ dyntftpd/cli.py
@@ -3,14 +3,20 @@ import argparse
 from .server import TFTPServer
 
 
-def main():
+def arguments_parser():
     parser = argparse.ArgumentParser(
         description='Extendable TFTP server, implemented in Python'
     )
     parser.add_argument('--host', '-H', default='')
     parser.add_argument('--port', '-p', default=69, type=int)
-    parser.add_argument('--root', '-r', default='/var/lib/tftpboot/')
-    args = parser.parse_args()
+    parser.add_argument(
+        '--root', '-r', default='/var/lib/tftpboot/', help='TFTP root folder'
+    )
+    return parser
 
+
+def main():
+    parser = arguments_parser()
+    args = parser.parse_args()
     tftp_server = TFTPServer(args.host, args.port, root=args.root)
     tftp_server.serve_forever()
