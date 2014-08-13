afbfb96d04ed8c13beb233fb710b588695262568
Julien Castets <castets.j@gmail.com>
2014-08-13 12:08:17 +0200

    Make handler a constructor argument of TFTPServer

--- dyntftpd/server.py
+++ dyntftpd/server.py
@@ -166,16 +166,15 @@ class FileSystemHandler(TFTPUDPHandler):
 
 class TFTPServer(SocketServer.UDPServer):
 
-    handler = FileSystemHandler
-
-    def __init__(self, host='', port=69, root='/tftboot'):
+    def __init__(self, host='', port=69, root='/tftboot',
+                 handler=FileSystemHandler):
 
         if not os.path.isdir(root):
             raise ValueError("'%s' is not a valid directory" % root)
 
         self.sessions = {}
         self.root = root
-        SocketServer.UDPServer.__init__(self, (host, port), self.handler)
+        SocketServer.UDPServer.__init__(self, (host, port), handler)
 
 
 def main():
