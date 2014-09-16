39527d16bc854e11e3a9a312a06b897b846df4a9
Julien Castets <castets.j@gmail.com>
2014-09-16 17:28:53 +0200

    Accept ../ in -r argument

--- dyntftpd/server.py
+++ dyntftpd/server.py
@@ -196,7 +196,7 @@ class FileSystemHandler(TFTPUDPHandler):
     def sanitize_filename(self, filename):
         """ Raise if trying to open a file up to the root folder.
         """
-        server_root = self.server.root
+        server_root = os.path.abspath(self.server.root)
         abs_path = os.path.abspath(os.path.join(server_root, filename))
         if os.path.commonprefix([abs_path, server_root]) != server_root:
             raise ValueError('Directory trasversal prevented')
