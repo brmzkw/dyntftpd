10e9c19c70cb6a0bd2e78702972db61ae77cb93b
Julien Castets <castets.j@gmail.com>
2014-10-30 10:12:30 +0100

    Typo

--- dyntftpd/server.py
+++ dyntftpd/server.py
@@ -248,7 +248,7 @@ class FileSystemHandler(TFTPUDPHandler):
         server_root = os.path.abspath(self.server.root)
         abs_path = os.path.abspath(os.path.join(server_root, filename))
         if os.path.commonprefix([abs_path, server_root]) != server_root:
-            raise ValueError('Directory trasversal prevented')
+            raise ValueError('Directory traversal prevented')
         return abs_path
 
     def load_file(self, filename):
