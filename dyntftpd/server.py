eaa77d76da86e392c6e84dfc46c5917b3df88d08
Julien Castets <castets.j@gmail.com>
2014-11-03 18:18:24 +0100

    Add argument handler_args to TFTPServer
    Handlers can lookup their configuration, with self.server.handler_args.

--- dyntftpd/server.py
+++ dyntftpd/server.py
@@ -4,10 +4,15 @@ from .handlers.fs import FileSystemHandler
 
 
 class TFTPServer(SocketServer.ThreadingMixIn, SocketServer.UDPServer):
+    """ Accepts the same arguments than SocketServer.UDPServer.
 
+    Can also provide `handler_args`, a dictionary used by handlers to lookup
+    their configuration.
+    """
     def __init__(self, host='', port=69, root='/var/lib/tftpboot',
-                 handler=FileSystemHandler):
+                 handler=FileSystemHandler, handler_args=None):
 
         self.sessions = {}
         self.root = root
+        self.handler_args = handler_args or {}
         SocketServer.UDPServer.__init__(self, (host, port), handler)
