ae59b574827130edfbcc229ee68abd8df7eb72d7
Julien Castets <castets.j@gmail.com>
2015-09-30 11:55:32 +0200

    Fix import error

--- dyntftpd/__init__.py
+++ dyntftpd/__init__.py
@@ -1,11 +1,13 @@
+__version__ = '0.4.1'
+
+import logging
+
 try:  # Python 2.7+
     from logging import NullHandler
 except ImportError:
     class NullHandler(logging.Handler):
         def emit(self, record):
             pass
-
-__version__ = '0.4.1'
 
 # Prevent message "No handlers could be found for logger "dyntftpd"" to be
 # displayed
